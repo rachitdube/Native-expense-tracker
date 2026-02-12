e028eedb20967b0b685fb4999c42743bfe9d69f4
Rachit <rachitdubey37@gmail.com>
2026-02-12 17:22:27 +0530

    debugging alert

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,7 +2,10 @@ import { Stack } from "expo-router";
 import SafeScreen from "@/components/SafeScreen";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import { StatusBar } from "react-native";
+
+ErrorUtils.setGlobalHandler((error, isFatal) => {
+  alert("Crash: " + error.message);
+});
 
 export default function RootLayout() {
   return (
